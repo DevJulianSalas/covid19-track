3640950a95960c669d926dc91cce6f92c18ff428
Julian Salas <juliansalas080@gmail.com>
2020-04-17 23:30:02 +0200

    updated firebase config

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ let db = admin.firestore();
 
 //const
 const URL_COVID = 'https://www.datos.gov.co/resource/gt2j-8ykr.json'
-
+const URL_METADATA = 'https://www.datos.gov.co/api/views/metadata/v1/gt2j-8ykr'
 
 //axios helper
 const getDataApi = async(url) => {
@@ -30,24 +30,77 @@ const getDataApi = async(url) => {
   }
 }
 
+const getMetadataApi = async(url) => {
+  try {
+    const { status, data } = await axios.get(url, {})
+    if (status === 200) {
+      return data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//helpers
+const setCovidData = async() => {
+  try {
+    const data = await getDataApi(URL_COVID)
+    if (data) {
+      data.forEach(async(document) => {
+          await db.collection('covid-data').doc(document.id_de_caso).set(document)
+      })
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
 //params cloud function
 const covidFunctionParms = {
-  timeoutSeconds: 200,
+  timeoutSeconds: 500,
   memory: '256MB'
 }
 
 //cloud functions
 exports.getCovidDataApi = functions
   .runWith(covidFunctionParms)
-  .pubsub.schedule('0 12 * * *')
-    .timeZone('America/Bogota')
-    .onRun(async(context) => {
-      const covidData = await getDataApi(URL_COVID)
-      if (covidData) {
-        covidData.forEach(async(document) => {
-          await db.collection('covid-data').doc(document.id_de_caso).set(document)
-        })
+  .firestore.document('covidmetadata/{covidmetadataId}').onCreate((snap, context) => {
+  var message = snap.data();
+  return db.collection('covidmetadata')
+    .orderBy('timestampUpdatedAt', 'DESC')
+    .limit(1)
+    .get()
+    .then(snapshot => {
+      if(!snapshot.empty){
+        snapshot.forEach(async(doc) => {
+          console.log(message.timestampUpdatedAt.toDate())
+          console.log(doc.data().timestampUpdatedAt.toDate())
+          if (message.timestampUpdatedAt.toDate() > doc.data().timestampUpdatedAt.toDate()){
+            setCovidData()
+          } else {
+            console.log('no hay actualizaciones')
+          }
+        });
       }
     })
+})
 
+exports.getMetadataCovid = functions
+  .runWith(covidFunctionParms)
+  .pubsub.schedule('0 12 * * *')
+  .onRun(async(context) => {
+    const covidMeta = await getMetadataApi(URL_METADATA)
+    if (covidMeta) {
+      const { 
+        id,
+        name,
+        dataUpdatedAt,
+        dataUri,
+        domain
+        } = covidMeta
+      const timestampUpdatedAt = admin.firestore.Timestamp.fromDate(new Date(dataUpdatedAt))
+      return db.collection('covidmetadata').doc().set({id, name, timestampUpdatedAt, dataUri, domain})
+    }
+    return 'Has ocurred an error get data from soda api'
+  })
